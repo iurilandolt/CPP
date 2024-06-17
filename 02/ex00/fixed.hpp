b147645e7ececdd6ea48e7c6c67aaeb5da38cb86
iuri~ <rlandolt@student.42lisboa.com>
2024-06-17 12:47:29 +0100

    starting CPP module 02

---
+++ 02/ex00/fixed.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlandolt <rlandolt@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/17 12:31:55 by rlandolt          #+#    #+#             */
+/*   Updated: 2024/06/17 12:31:56 by rlandolt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+    public:
+
+    private:
+        int              _value;
+        static const int _fBits;
+};
+
+#endif
