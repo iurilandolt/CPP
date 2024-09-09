096fa2a8c9d1740b5b28a22fc79ca75e2e2f4a4d
iuri- <123178126+iurilandolt@users.noreply.github.com>
2024-09-09 14:29:33 +0100

    preparing input parsing

--- 06/ex00/ScalarConverter.cpp
+++ 06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: rlandolt <rlandolt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 11:09:33 by rlandolt          #+#    #+#             */
-/*   Updated: 2024/09/09 12:13:21 by rlandolt         ###   ########.fr       */
+/*   Updated: 2024/09/09 14:27:09 by rlandolt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,50 @@
 ScalarConverter::ScalarConverter() {
 }
 
-ScalarConverter::ScalarConverter(ScalarConverter const & src) {
-	*this = src;
-}
-
-ScalarConverter & ScalarConverter::operator=(ScalarConverter const & rhs) {
-	return *this;
-}
-
 ScalarConverter::~ScalarConverter() {
 }
 
 void ScalarConverter::convert(std::string const & str) {
+	std::string tmp = str;
+	std::string::iterator it = tmp.begin();
+	bool fractional = false;
+	
+	if (tmp.length() > 1) {
+		while (it != tmp.end() && *it == ' ') {
+			tmp.erase(it);
+			it++;
+		}
+		it = tmp.begin();
+		if (*it == '+' || *it == '-') {
+			it++;
+		}
+		if (!isdigit(*it)) {
+			std::cout << "Error: invalid input 1" << std::endl;
+			return ;
+		}
+		while (it != tmp.end() && isdigit(*it)) {
 
-	std::cout << "char: " << getChar(str) << std::endl;
+			it++;
+			if (*it == '.') {
+				if (fractional) {
+					std::cout << "Error: invalid input 2" << std::endl;
+					return ;
+				}
+				fractional = true;
+				it++;
+			}
+		}
+		if (it == tmp.end() && (*it != 'f' || !isdigit(*it))) {
+			std::cout << "Error: invalid input 3" << std::endl;
+			return ;
+		}
+ 
+	}
+	std::cout << "valid input: " << tmp << std::endl;
+	//std::cout << "char: " << getChar(str) << std::endl;
 	//std::cout << "int: " << getInt(str) << std::endl;
 	//std::cout << "float: " << getFloat(str) << std::endl;
 	//std::cout << "double: " << getDouble(str) << std::endl;
 }
 
-char ScalarConverter::getChar(std::string const & str) {
-	
-}
+/* ß */
--- 06/ex00/ScalarConverter.hpp
+++ 06/ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: rlandolt <rlandolt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 11:09:35 by rlandolt          #+#    #+#             */
-/*   Updated: 2024/09/09 12:13:23 by rlandolt         ###   ########.fr       */
+/*   Updated: 2024/09/09 14:27:00 by rlandolt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,12 @@ class ScalarConverter
 		void static convert(std::string const & str);
 	private:
 		ScalarConverter();
-		ScalarConverter(std::string const & str);
 		~ScalarConverter();
-		ScalarConverter & operator=(ScalarConverter const & rhs);
 
-		char static getChar(std::string const & str);
-		int getInt(std::string const & str);
-		float getFloat(std::string const & str);
-		double getDouble(std::string const & str);
+		//char static getChar(std::string const & str);
+		//int getInt(std::string const & str);
+		//float getFloat(std::string const & str);
+		//double getDouble(std::string const & str);
 };
 
 #endif
