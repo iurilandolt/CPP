0ce3d80df3968d0b7b60bfddfa2b61058f73f839
iuri- <123178126+iurilandolt@users.noreply.github.com>
2024-09-24 14:28:17 +0100

    implemented basic file reading and _database parsing/initialization

--- 09/ex00/BitcoinExchange.cpp
+++ 09/ex00/BitcoinExchange.cpp
@@ -6,32 +6,107 @@
 /*   By: rlandolt <rlandolt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 19:40:54 by rlandolt          #+#    #+#             */
-/*   Updated: 2024/09/23 20:37:10 by rlandolt         ###   ########.fr       */
+/*   Updated: 2024/09/24 14:27:09 by rlandolt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-BitcoinExchange::BitcoinExchange(const std::string &filename) : _database("data.csv") ,  _infile(filename) {
-	if (!_infile.is_open()) {
+BitcoinExchange::BitcoinExchange() : _database("data.csv") {}
+
+BitcoinExchange::BitcoinExchange(const std::string &filename) : 
+	_database("data.csv"),  _infile(filename.c_str()), _infile_name(filename) {
+		
+	if (!_infile.is_open() || !_infile.good() || _infile.bad() || _infile.fail())
 		throw std::runtime_error("BitcoinExchange: could not open input file");
-	}
-	if (!_database.is_open()) {
+	if (!_database.is_open() || !_database.good() || _database.bad() || _database.fail())
 		throw std::runtime_error("BitcoinExchange: could not open database file");
-	}
-	std::string line;
-	while (std::getline(_database, line)) {	
-		std::cout << line << std::endl;
-	}
+	_parseDB();
 }
 
 BitcoinExchange::~BitcoinExchange() {
+	_database.close();
+	_infile.close();
 }
 
-BitcoinExchange::BitcoinExchange() : _database("data.csv") {
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &src) {
+	*this = src;
 }
 
 BitcoinExchange &BitcoinExchange::operator=(BitcoinExchange const &src) {
-	(void)src;
+	if (this != &src) {
+		_exchange.clear();
+		_exchange = src._exchange;
+		_infile_name = src._infile_name;
+		if (_database.is_open())
+			_database.close();
+		_database.open("data.csv");
+		if (_infile.is_open())
+			_infile.close();
+		_infile.open(_infile_name.c_str());
+	}
 	return *this;
+}
+
+bool validate_day(int year, int month, int day)
+{
+	std::list<int> days_per_month;
+	bool leap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+	for (int i = 1; i <= 12; i++)
+	{
+		if (i == 2)
+		{
+			if (leap)
+				days_per_month.push_back(29);
+			else
+				days_per_month.push_back(28);
+		}
+		else if (i == 4 || i == 6 || i == 9 || i == 11)
+			days_per_month.push_back(30);
+		else
+			days_per_month.push_back(31);
+	}
+	std::list<int>::iterator it = days_per_month.begin();
+	std::advance(it, month - 1);
+	if (day < 1 || day > *it)
+		return false;
+	return true;
+}
+
+bool validate_format(const std::string &date)
+{
+	if (date.size() != 10 || date[10] != '\0')
+		return false;
+	int year, month, day;
+	char dash1, dash2;
+	std::istringstream iss(date);
+	if (!(iss >> year >> dash1 >> month >> dash2 >> day))
+	{
+		return false;
+	}
+	if (dash1 != '-' || dash2 != '-')
+		return false;
+	if (year < 0 || month < 1 || month > 12 || day < 1 || day > 31)
+		return false;
+	if (!validate_day(year, month, day))
+		return false;
+	return true;
+}
+
+void BitcoinExchange::_parseDB(void) {
+	std::string line;
+	//std::getline(_database, line); // we need to skip the header if we are using the else if statement
+	while (std::getline(_database, line)) {
+		std::istringstream iss(line);
+		std::string date;
+		double rate;
+		if (std::getline(iss, date, ',') && iss >> rate) {
+			if (!validate_format(date))
+				throw std::runtime_error("BitcoinExchange: invalid date format");
+			_exchange[date] = rate;
+			std::cout << date << "," << _exchange[date] << std::endl;
+		}
+		// else if (line.size() > 0)
+		// 	throw std::runtime_error("BitcoinExchange: invalid line format");
+	}
 }
--- 09/ex00/BitcoinExchange.hpp
+++ 09/ex00/BitcoinExchange.hpp
@@ -6,23 +6,33 @@
 /*   By: rlandolt <rlandolt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 19:41:03 by rlandolt          #+#    #+#             */
-/*   Updated: 2024/09/23 20:31:43 by rlandolt         ###   ########.fr       */
+/*   Updated: 2024/09/24 14:20:23 by rlandolt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <iterator>
+#include <cstdlib>
 #include <map>
+#include <list>
 
 class BitcoinExchange {
 	public:
 		BitcoinExchange(const std::string &filename);
+		BitcoinExchange(const BitcoinExchange &src);
 		~BitcoinExchange();
 		BitcoinExchange &operator=(BitcoinExchange const &src);
 	private:
 		BitcoinExchange();
-		//std::map<std::string, double> _exchange;
+		void _parseDB(void);
+		std::map<std::string, double> _exchange;
 		std::ifstream _database;
 		std::ifstream _infile;
+		std::string _infile_name;
 		//std::ofstream _outfile;
 };
+
+bool validate_format(const std::string &date);
+bool validate_day(int year, int month, int day);
